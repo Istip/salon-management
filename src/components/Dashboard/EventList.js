39cefb750b6dd2b933ac151a33e967b3c326be27
Istip <isticsek@gmail.com>
2021-12-16 20:42:39 +0200

    redesigned dashboard calendar, added new icons and view filterings

--- src/components/Dashboard/EventList.js
+++ src/components/Dashboard/EventList.js
@@ -2,25 +2,58 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import moment from 'moment';
-import { useTranslation } from 'react-i18next';
 import { tokens } from '../UI/tokens';
+import { timestamps } from '../../utils/timestamps';
 
 // project components
 import Event from './Event';
 import ModalAdd from './ModalAddEvent';
-import Text from '../UI/Text';
 import Button from '../UI/Button';
-import AddIcon from '../icons/AddIcon';
-import FlexCenter from '../UI/FlexCenter';
 import ModalPay from './ModalPay';
 import Error from '../UI/Error';
+import Placeholder from './Placeholder';
+import Text from '../UI/Text';
+import FlexCenter from '../UI/FlexCenter';
+import AddIcon from '../icons/AddIcon';
+import TimeIcon from '../icons/TimeIcon';
+import ViewAllIcon from '../icons/ViewAllIcon';
+import CalendarIcon from '../icons/CalendarIcon';
 
 const EventList = ({ events, error, selectedDate }) => {
   const [showAdd, setShowAdd] = useState(false);
   const [showPay, setShowPay] = useState(false);
   const [selected, setSelected] = useState(0);
 
-  const { t } = useTranslation();
+  // state responsible for the rendered view
+  const [active, setActive] = useState('working-hours');
+  const [workingHours, setWorkingHours] = useState([16, 37]);
+
+  // Function returning if the passed number is odd or not
+  const isOdd = (number) => {
+    return number % 2;
+  };
+
+  // Filter button selecting
+  const viewAll = () => {
+    if (active !== 'all') {
+      setActive('all');
+      setWorkingHours([0, 48]);
+    }
+  };
+
+  const viewFiltered = () => {
+    if (active !== 'filtered') {
+      setActive('filtered');
+      setWorkingHours([0, 48]);
+    }
+  };
+
+  const viewWorkingHours = () => {
+    if (active !== 'working-hours') {
+      setActive('working-hours');
+      setWorkingHours([16, 37]);
+    }
+  };
 
   // Check if the date is after or the same as current day
   const checkDate =
@@ -31,6 +64,24 @@ const EventList = ({ events, error, selectedDate }) => {
     return null;
   }
 
+  // Constant returning the mix of two arrays:
+  // ... imported timestamps which holds the daily calendar timestamps
+  // ... if the timestamp has match with an event, it replaces the timestamp
+  // ... and holds the data of the event instead
+  const dailyData =
+    events &&
+    timestamps.map(
+      (obj) =>
+        events.find(
+          (o) => moment(o.date.seconds * 1000).format('HH:mm') === obj
+        ) || obj
+    );
+
+  // Return the number of daily clients
+  const clientsForDay = dailyData
+    .filter((client) => typeof client !== 'string')
+    .map((client) => client.finished);
+
   const nextEvent = events.find(
     (event) =>
       moment(event.date.seconds * 1000).format('YY-MM-DD HH:mm') >
@@ -39,45 +90,73 @@ const EventList = ({ events, error, selectedDate }) => {
   );
 
   return (
-    <EventListWrapper>
+    <>
       <AddEvent>
-        <Text tag="h2" variant="h2" color={tokens.colors.primaryDark3}>
-          {t('dashboard.appointments')}
-        </Text>
-        {checkDate && (
-          <Button
-            onClick={() => setShowAdd(!showAdd)}
-            icon={<AddIcon color={tokens.colors.fff} />}
-            rounded
-          />
-        )}
+        <FilterMenu>
+          {!!clientsForDay.length && (
+            <ClientsBadge
+              finished={clientsForDay.every((item) => item === true)}
+            >
+              <Text variant="regular8" color={tokens.colors.fff}>
+                {clientsForDay.length}
+              </Text>
+            </ClientsBadge>
+          )}
+
+          <FlexCenter style={{ gap: '5px' }}>
+            <FilterItem
+              active={active === 'working-hours'}
+              onClick={() => viewWorkingHours()}
+            >
+              <FlexCenter>
+                <TimeIcon color={tokens.colors.primaryDark3} />
+              </FlexCenter>
+            </FilterItem>
+
+            <FilterItem
+              active={active === 'filtered'}
+              onClick={() => viewFiltered()}
+            >
+              <FlexCenter>
+                <ViewAllIcon color={tokens.colors.primaryDark3} />
+              </FlexCenter>
+            </FilterItem>
+
+            <FilterItem active={active === 'all'} onClick={() => viewAll()}>
+              <FlexCenter>
+                <CalendarIcon color={tokens.colors.primaryDark3} />
+              </FlexCenter>
+            </FilterItem>
+          </FlexCenter>
+        </FilterMenu>
+
+        <Button
+          disabled={!checkDate}
+          onClick={() => setShowAdd(!showAdd)}
+          icon={<AddIcon color={tokens.colors.fff} />}
+          rounded
+        />
 
         {error && <Error>{error}</Error>}
       </AddEvent>
 
-      <EventWrapper>
-        {events.length ? (
-          <>
-            {events
-              .sort((a, b) => a.date.seconds - b.date.seconds)
-              .map((event) => (
-                <Event
-                  key={event.id}
-                  event={event}
-                  setSelected={setSelected}
-                  setShowPay={setShowPay}
-                  next={nextEvent}
-                />
-              ))}
-          </>
-        ) : (
-          <FlexCenter>
-            <Text variant="regular14" color={tokens.colors.primaryLight3}>
-              {t('warning.no_appointments')}
-            </Text>
-          </FlexCenter>
-        )}
-      </EventWrapper>
+      {dailyData.slice(workingHours[0], workingHours[1]).map((event, i) => {
+        return (
+          <EventItem key={i}>
+            {typeof event !== 'string' ? (
+              <Event
+                event={event}
+                setSelected={setSelected}
+                setShowPay={setShowPay}
+                next={nextEvent}
+              />
+            ) : (
+              <>{active !== 'filtered' && <Placeholder event={event} />}</>
+            )}
+            {isOdd(i) && active !== 'filtered' ? <Divider /> : null}
+          </EventItem>
+        );
+      })}
 
       <ModalAdd
         show={showAdd}
@@ -86,14 +165,17 @@ const EventList = ({ events, error, selectedDate }) => {
         setSelected
       />
       <ModalPay show={showPay} setShow={setShowPay} selected={selected} />
-    </EventListWrapper>
+    </>
   );
 };
 
 // styled components
-const EventListWrapper = styled.div``;
+const EventItem = styled.div``;
 
-const EventWrapper = styled.div``;
+const Divider = styled.div`
+  width: 100%;
+  height: 10px;
+`;
 
 const AddEvent = styled.div`
   width: 100%;
@@ -101,6 +183,57 @@ const AddEvent = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+`;
+
+const FilterMenu = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 5px;
+  background: ${tokens.colors.primaryLight4};
+  border-radius: 4px;
+  border: 1px solid ${tokens.colors.primaryLight3};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  padding: 6px;
+  gap: 6px;
+`;
+
+const ClientsBadge = styled.div`
+  position: absolute;
+  right: -8px;
+  top: -8px;
+  border-radius: 20px;
+  width: 16px;
+  height: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: ${(props) =>
+    props.finished ? `${tokens.colors.success}` : `${tokens.colors.warning}`};
+  border: ${(props) =>
+    props.finished
+      ? `1px solid ${tokens.colors.success}`
+      : `1px solid ${tokens.colors.warning}`};
+`;
+
+const FilterItem = styled.span`
+  background: ${(props) =>
+    props.active
+      ? `${tokens.colors.primaryLight3}`
+      : `${tokens.colors.primaryLight4}`};
+  padding: 6px;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: 250ms ease;
+
+  &:hover {
+    background: ${tokens.colors.primaryLight3};
+  }
 `;
 
 export default EventList;
---
+++ src/components/Dashboard/Placeholder.js
@@ -0,0 +1,120 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { tokens } from '../UI/tokens';
+
+// Project imports
+import Text from '../UI/Text';
+import AddIcon from '../icons/AddIcon';
+import FlexCenter from '../UI/FlexCenter';
+
+const Placeholder = ({ event }) => {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <PlaceholderWrapper>
+      <PlaceholderInfo>
+        <PlaceholderTime>
+          <Text
+            tag="div"
+            variant="medium10"
+            color={tokens.colors.primaryLight2}
+          >
+            {event}
+          </Text>
+        </PlaceholderTime>
+        <PlaceholderCard
+          onClick={() => console.log(event)}
+          onMouseEnter={() => setVisible(true)}
+          onMouseLeave={() => setVisible(false)}
+        >
+          <FlexCenter style={{ gap: '4px' }}>
+            <AddIcon color={tokens.colors.primaryLight2} />
+            <HiddenTime className={visible ? 'visible' : ''}>
+              <Text variant="regular14" color={tokens.colors.primaryLight2}>
+                {event}
+              </Text>
+            </HiddenTime>
+          </FlexCenter>
+        </PlaceholderCard>
+      </PlaceholderInfo>
+    </PlaceholderWrapper>
+  );
+};
+
+// Styled components
+const PlaceholderWrapper = styled.div`
+  margin: 0 10px;
+`;
+
+const PlaceholderInfo = styled.div`
+  width: 100%;
+  display: flex;
+  min-height: 60px;
+  margin: 5px 0;
+`;
+
+const PlaceholderTime = styled.div`
+  position: relative;
+  min-width: 50px;
+  max-width: 50px;
+  display: flex;
+  justify-content: center;
+  padding: 10px;
+
+  /* monospace the numbers */
+  font-feature-settings: 'tnum' on, 'lnum' on;
+`;
+
+const PlaceholderCard = styled.div`
+  width: 100%;
+  height: 70px;
+  border-radius: 0 12px 12px 0;
+  border: 1px dashed ${tokens.colors.primaryLight3};
+  border-left: 3px solid ${tokens.colors.primaryLight3};
+  cursor: pointer;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background-image: linear-gradient(
+    45deg,
+    #ffffff 25%,
+    #fafcfe 25%,
+    #fafcfe 50%,
+    #ffffff 50%,
+    #ffffff 75%,
+    #fafcfe 75%,
+    #fafcfe 100%
+  );
+  background-size: 30px 30px;
+  opacity: 0.75;
+  transition: 250ms ease;
+
+  &:hover {
+    box-shadow: 0 0 10px 0 rgba(14, 44, 77, 0.15);
+    opacity: 1;
+  }
+`;
+
+const HiddenTime = styled.div`
+  max-width: 0;
+  overflow: hidden;
+  transition: 250ms ease;
+
+  /* monospace the numbers */
+  font-feature-settings: 'tnum' on, 'lnum' on;
+
+  &.visible {
+    max-width: 40px;
+  }
+`;
+
+export default Placeholder;
+
+// Prop types
+
+Placeholder.propTypes = {
+  event: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+};
