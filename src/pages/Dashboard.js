70218a63d12104fe117c7597c7f97468d68f443d
Istip <isticsek@gmail.com>
2023-02-18 13:24:49 +0200

    update dashboard to fetch only events from last week to future

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,27 +1,33 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import moment from 'moment';
-import { useCollection } from '../hooks/useCollection';
+import React, { useState } from "react";
+import styled from "styled-components";
+import moment from "moment";
+import firebase from "firebase/app";
+import { useCollection } from "../hooks/useCollection";
 
 // project components
-import Calendar from '../components/Dashboard/Calendar';
-import EventsList from '../components/Dashboard/EventsList';
-import Error from '../components/UI/Error';
-import Animate from '../components/UI/Animate';
+import Calendar from "../components/Dashboard/Calendar";
+import EventsList from "../components/Dashboard/EventsList";
+import Error from "../components/UI/Error";
+import Animate from "../components/UI/Animate";
 
 const Dashboard = () => {
   const [selectedDate, setSelectedDate] = useState(moment());
 
-  const { documents, error } = useCollection('events');
+  const days = new Date();
+  days.setDate(days.getDate() - 5);
+
+  const date = firebase.firestore.Timestamp.fromDate(days);
+
+  const filterOldEventsQuery = ["date", ">=", date];
+
+  const { documents, error } = useCollection("events", filterOldEventsQuery);
 
   // Filter the incoming data for match the current day
-  const filteredEvents =
-    documents &&
-    documents.filter(
-      (doc) =>
-        moment(doc.date.seconds * 1000).format('YY-MM-DD') ===
-        moment(selectedDate).format('YY-MM-DD')
-    );
+  const filteredEvents = documents?.filter(
+    (doc) =>
+      moment(doc.date.seconds * 1000).format("YY-MM-DD") ===
+      moment(selectedDate).format("YY-MM-DD")
+  );
 
   return (
     <Animate>
