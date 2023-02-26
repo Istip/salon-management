6d69f9b2efac3b5486bf0a6c19d5533ddbd87157
Istip <isticsek@gmail.com>
2023-02-26 14:37:42 +0200

    update sign up to change the actions type

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
-import { projectAuth, projectFirestore } from '../firebase/config';
-import { useAuthContext } from './useAuthContext';
+import { useEffect, useState } from "react";
+import { projectAuth, projectFirestore } from "../firebase/config";
+import { useAuthContext } from "./useAuthContext";
 
 export const useSignup = () => {
   const [cancelled, setCancelled] = useState(false);
@@ -22,7 +22,7 @@ export const useSignup = () => {
 
       // handling if sign up fails
       if (!res) {
-        throw new Error('Could not complete the sign up!');
+        throw new Error("Could not complete the sign up!");
       }
 
       // updating the user
@@ -30,7 +30,7 @@ export const useSignup = () => {
 
       // create document with the user
       await projectFirestore
-        .collection('users')
+        .collection("users")
         .doc(res.user.uid)
         .set({
           uid: res.user.uid,
@@ -39,12 +39,16 @@ export const useSignup = () => {
           admin: false,
           displayName: name,
           email: email,
-          membership: 'basic',
-          actions: ['haircut', 'hairdye', 'other'],
+          membership: "basic",
+          actions: [
+            { name: "haircut", price: 0 },
+            { name: "hairdye", price: 0 },
+            { name: "other", price: 0 },
+          ],
         });
 
       // dispatching login action
-      dispatch({ type: 'LOGIN', payload: res.user });
+      dispatch({ type: "LOGIN", payload: res.user });
 
       // update state
       if (!cancelled) {
