9a2b98a7222d64a7afc4ed8cee0fe708b1c95df7
Istip <isticsek@gmail.com>
2023-02-28 19:28:06 +0200

    remove async await

--- src/components/Dashboard/Event.js
+++ src/components/Dashboard/Event.js
@@ -29,7 +29,7 @@ const Event = ({ event, user }) => {
   const { t } = useTranslation();
 
   // Function returning different action for button, based on finished status
-  const handleDeleteButton = async (event) => {
+  const handleDeleteButton = (event) => {
     setVisible(false);
     navigator.vibrate(100);
 
@@ -45,7 +45,7 @@ const Event = ({ event, user }) => {
       );
     }
 
-    await updateDocument(event.id, {
+    updateDocument(event.id, {
       ...event,
       finished: false,
       price: 0,
@@ -53,7 +53,7 @@ const Event = ({ event, user }) => {
   };
 
   // Function to finish an event with the given price
-  const handleFinish = async (event) => {
+  const handleFinish = (event) => {
     navigator.vibrate(100);
 
     const same = user.actions.filter(
@@ -61,7 +61,7 @@ const Event = ({ event, user }) => {
     );
     const finalPrice = same.length ? Number(same[0].price) : 0;
 
-    await updateDocument(event.id, {
+    updateDocument(event.id, {
       ...event,
       finished: true,
       price: finalPrice,
