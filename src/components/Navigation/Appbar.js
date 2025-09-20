4142badf6af314d1d02752485e38663ec37b5181
Pasztor Isti <isticsek@gmail.com>
2025-09-20 16:09:11 +0300

    add navigation to the appbar

--- src/components/Navigation/Appbar.js
+++ src/components/Navigation/Appbar.js
@@ -1,17 +1,24 @@
-import React, { useEffect, useRef, useState } from 'react';
-import styled from 'styled-components';
-import { useAuthContext } from '../../hooks/useAuthContext';
-import { tokens } from '../UI/tokens';
+import { useEffect, useRef, useState } from "react";
+import styled from "styled-components";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { tokens } from "../UI/tokens";
 
-import Hamburger from '../icons/Hamburger';
-import FlexCenter from '../UI/FlexCenter';
-import Logo from '../UI/Logo';
-import AppbarPopover from './AppbarPopover';
+import Hamburger from "../icons/Hamburger";
+import FlexCenter from "../UI/FlexCenter";
+import Logo from "../UI/Logo";
+import AppbarPopover from "./AppbarPopover";
+import ClientsIcon from "../icons/ClientsIcon";
+import { Link, useLocation } from "react-router-dom";
+import NotesIcon from "../icons/NotesIcon";
 
 const Appbar = () => {
   const [visible, setVisible] = useState(false);
 
+  const { pathname } = useLocation();
+
   const wrapperNode = useRef();
+
+  const { primary, darkGrey } = tokens.colors;
 
   const { user } = useAuthContext();
 
@@ -24,11 +31,11 @@ const Appbar = () => {
 
   useEffect(() => {
     if (wrapperNode) {
-      document.addEventListener('mousedown', handleClickOutside);
+      document.addEventListener("mousedown", handleClickOutside);
     }
 
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
 
@@ -38,8 +45,24 @@ const Appbar = () => {
 
       {user && (
         <UserTab>
-          <FlexCenter onClick={() => setVisible(true)}>
-            <Hamburger color={`${tokens.colors.darkGrey}`} />
+          <FlexCenter>
+            <MenuItemWrapper padding={4}>
+              <Link to="/clients">
+                <ClientsIcon
+                  color={pathname === "/clients" ? primary : darkGrey}
+                />
+              </Link>
+            </MenuItemWrapper>
+            <MenuItemWrapper padding={4}>
+              <Link to="/reports">
+                <NotesIcon
+                  color={pathname === "/reports" ? primary : darkGrey}
+                />
+              </Link>
+            </MenuItemWrapper>
+            <span onClick={() => setVisible(true)} style={{ marginLeft: 16 }}>
+              <Hamburger color={`${tokens.colors.darkGrey}`} />
+            </span>
           </FlexCenter>
           <span ref={wrapperNode}>
             {visible && <AppbarPopover user={user} setVisible={setVisible} />}
@@ -51,6 +74,10 @@ const Appbar = () => {
 };
 
 // Styled components
+
+const MenuItemWrapper = styled.div`
+  padding: ${({ padding }) => (padding ? `0 ${padding}px` : "0 16px")};
+`;
 
 const AppbarWrapper = styled.div`
   position: fixed;
